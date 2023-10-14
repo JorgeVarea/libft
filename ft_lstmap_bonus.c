8220fdf7311b42c18230f8202cf4156b8177f95e
JorgeVarea <vareaduranjorge@gmail.com>
2023-10-14 20:22:24 +0200

    Upload lstmap
    Trying to fix lstmap

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: jorvarea <jorvarea@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 11:28:24 by jorvarea          #+#    #+#             */
-/*   Updated: 2023/10/11 12:00:21 by jorvarea         ###   ########.fr       */
+/*   Updated: 2023/10/14 20:18:48 by jorvarea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,21 @@
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_lst;
-	t_list	*start_lst;
-	t_list	*start_new_lst;
+	t_list	*new_element;
 
 	if (!lst || !f || !del)
 		return (NULL);
-	start_lst = lst;
-	start_new_lst = malloc(ft_lstsize(lst) * sizeof(t_list));
-	if (!start_new_lst)
-		return (NULL);
+	new_lst = NULL;
 	while (lst)
 	{
-		new_lst = ft_lstnew(f(lst->content));
-		ft_lstadd_back(&start_new_lst, new_lst);
+		new_element = ft_lstnew(f(lst->content));
+		if (!new_element)
+		{
+			ft_lstclear(&new_lst, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&new_lst, new_element);
 		lst = lst->next;
 	}
-	ft_lstclear(&start_lst, del);
-	return (start_new_lst);
+	return (new_lst);
 }
